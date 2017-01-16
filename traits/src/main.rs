c9d1d2a99dceb56c261a67fe4ca6179ab4ce001a
Bryan Mytko <bryanmytko@gmail.com>
2017-01-16 05:33:47 -0500

    Rust notes: Traits bound on generic functions

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,25 +1,99 @@
 // #[derive(Display)]
-enum Animal {
-    Cow,
-    Dog,
-    Cat
+#[allow(dead_code)]
+
+pub struct Animal {
+    pub name: &'static str,
+    pub age: i32,
+}
+
+pub struct Person {
+    name: &'static str,
+    age: i32,
+}
+
+/* We can add `new` method directly: */
+//
+// impl Animal {
+//     pub fn new(name: &'static str, age: i32) -> Animal {
+//         Animal {
+//             name: name,
+//             age: age,
+//         }
+//     }
+// }
+//
+// impl Person {
+//     pub fn new(name: &'static str, age: i32) -> Animal {
+//         Animal {
+//             name: name,
+//             age: age,
+//         }
+//     }
+// }
+
+/* Or use a construct trait? */
+//
+trait Construct {
+    fn new(name: &'static str, age: i32) -> Self;
+}
+
+impl Construct for Animal {
+    fn new(name: &'static str, age: i32) -> Self {
+        Animal {
+            name: name,
+            age: age,
+        }
+    }
+}
+
+impl Construct for Person {
+    fn new(name: &'static str, age: i32) -> Self {
+        Person {
+            name: name,
+            age: age,
+        }
+    }
+}
+
+// Implement Speak trait for Animal and Person
+trait Speak {
+    fn say_name(&self) -> &'static str;
+    fn say_age(&self) -> i32;
+}
+
+impl Speak for Animal {
+    fn say_name(&self) -> &'static str {
+        self.name
+    }
+
+    fn say_age(&self) -> i32 {
+        self.age
+    }
+}
+
+impl Speak for Person {
+    fn say_name(&self) -> &'static str {
+        self.name
+    }
+
+    fn say_age(&self) -> i32 {
+        self.age
+    }
 }
 
 fn main() {
-  let my_cow = Animal::Cow;
-  let my_dog = Animal::Dog;
-  let my_cat = Animal::Cat;
-  paws(my_cow);
-  paws(my_dog);
-  paws(my_cat);
+    let a = Animal::new("Dexter", 4);
+    let b = Person::new("Bryan", 34);
+
+    greet(a);
+    greet(b);
 }
 
-fn paws(animal: Animal){
-    use Animal::*;
-
-    println!("{}", match animal {
-        Cow => 0,
-        Dog => 4,
-        Cat => 4
-    });
+/* Greet function that accepts any type that implements Speak */
+fn greet<T: Speak>(being: T){
+    println!(
+        "Hello, I am {} and am {} years old.",
+        being.say_name(),
+        being.say_age()
+    );
 }
